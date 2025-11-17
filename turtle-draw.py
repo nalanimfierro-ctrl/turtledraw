432e9e61b06c0ae8be3c21dcd7267d2b69455186
Nalani Fierro <nalanimfierro@lewisu.edu>
2025-11-16 21:03:42 -0600

    updatefile

--- turtle-draw.py
+++ turtle-draw.py
@@ -1,27 +1,18 @@
 import turtle
 
 TEXTFILE = 'line-data.txt'
-print('Turtle Draw Lite Starting...')
-'''
-edTheTurtle = turtle.Turtle ()
-edTheTurtle.forward(100)
-edTheTurtle.right(90)
-edTheTurtle.forward(100)
-edTheTurtle.right(90)
-edTheTurtle.forward(100)
-'''
-turtleDraw = turtle.Turtle()
-turtleDraw.speed(5)
-turtleDraw.penup()
-'''
-for i in range (40):
-    turtleDraw.forward(i * 10)
-    turtleDraw.right(144)
-'''
-print("")
-print("Turtle Draw -Part 2")
 
-lineDataTextfile = open(TEXTFILENAME,"line.data.txt")
+print('Turtle Draw Lite Starting...')
+
+turtleDraw = turtle.Turtle()
+turtleDraw.speed(0)
+turtleDraw.penup()
+
+print(" ")
+
+
+
+lineDataTextfile = open(TEXTFILENAME, "r")
 line = lineDataTextfile.readline()
    
 while line:
@@ -29,7 +20,7 @@ while line:
     line = lineDataText.file.redline()
     parts = line.split(' ')
 
-if len(parts) ==3:
+if len(parts) == 3:
         color = parts[0]
         x = int(parts[1])
         y = int(parts[2])
@@ -38,9 +29,10 @@ if len(parts) ==3:
         turtleDraw.goto(x, y)
         turtleDraw.pendown()
 
-if len(parts) ==1:
+if len(parts) == 1:
         turtle.Draw.penup()
 
-turtle.done
+turtle.done()
+turtleDraw.txtfile.close()
 print("\nEnd")        
 
